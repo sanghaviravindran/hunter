d303874fe319f547370f164eb3eb988d87b48b4c
sanghaviravindran <sangu.abirami@gmail.com>
2016-07-18 14:49:25 +0530

    Add files via upload

---
+++ first elemen1t.java
@@ -0,0 +1,20 @@
+ public static void main(String[] args) {
+        int[] a1={1,2,3,10,9,4,5,7,7,7,7,8};
+        int i,flag1 = 0,j;
+        for(i=0;i<=11;i++)
+        {
+            for(j=i+1;j<11;j++)
+            {
+                if(flag1==1)
+                    continue;
+                if(a[i]==a[j])
+              {
+                flag1=1;
+System.out.println("the First element which is   repeated is"+a1[i]);
+                break;
+         }}
+               
+                    
+            
+        }
+   
