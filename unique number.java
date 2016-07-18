37d6ef84ec732c0fde2e2a18be465782a7f3a2d3
sanghaviravindran <sangu.abirami@gmail.com>
2016-07-18 15:10:23 +0530

    Update unique number.java

--- unique number.java
+++ unique number.java
@@ -1,15 +1,15 @@
 for(i=0;i<=11;i++)
         {
-            flag1=0;
+            flag2=0;
             for(j=0;j<=11;j++)
             {
                 if(i==j)
                     continue;
             if(a1[i]==a1[j]){
-                flag1=1;
+                flag2=1;
                 break;
             }}
-                if(flag1!=1)
+                if(flag2!=1)
                     System.out.println("the no"+a1[i]+"is unique in the array");
             
         }
