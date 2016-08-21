e6e20a3e7ecb4db893bf27cea534eb2582721256
sanghaviravindran <sangu.abirami@gmail.com>
2016-08-21 16:54:36 +0530

    Add files via upload

---
+++ no_of_2s_in_range.java
@@ -0,0 +1,6 @@
+int i,count=0,n,n1;
+         for( i=0;i<100;i++){
+                n=i%10;n1=i/10;
+                if(n==2||n1==2)
+                count++;}
+System.out.println("count is:"+count);
