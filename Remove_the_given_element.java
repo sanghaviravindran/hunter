9e3b87edbfbb9fb5811433519fa059009c80d77d
sanghaviravindran <sangu.abirami@gmail.com>
2016-08-30 20:48:00 +0530

    Add files via upload

---
+++ Remove_the_given_element.java
@@ -0,0 +1,9 @@
+ n=sc.nextInt();
+      for(int i=0;i<a.length;i++)
+      {
+          if(a[i]==n){
+              a[i]=0;}
+          if(a[i]==0){
+          continue;}
+              System.out.print(" "+a[i]);
+      }
